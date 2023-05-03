a0dd69e937315809da611cf4223a9b6e9e8edcc0
huulai2510 <huulai2510@gmail.com>
2023-05-03 18:10:36 +0700

    added graphql and user feature

---
+++ libs/api/feature-user/src/lib/user.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserResolver } from './user.resolver';
+import { PrismaModule } from '@api/api/data-access-db';
+
+@Module({
+  providers: [UserResolver, UserService],
+  imports: [
+    PrismaModule
+  ]
+})
+export class UserModule {}
